6a5124dee4aaf1f6b8a948b06b9034695b486ef4
zonble <zonble@gmail.com>
2022-03-09 01:00:28 +0800

    Adds comments.

--- lib/src/window_position.dart
+++ lib/src/window_position.dart
@@ -1,4 +1,8 @@
+/// Represents the desired position of the alert window.
 enum AlertWindowPosition {
+  /// The alert window should be placed at the center of the parent window.
   parentWindowCenter,
+
+  /// The alert window should be placed at the center of the screen.
   screenCenter,
 }
